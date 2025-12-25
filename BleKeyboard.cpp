82913840a87980f6bb5eda7fb7cbeeda5e0a492c
fromthink <8228841@qq.com>
2025-12-25 19:00:15 +0800

    Update usage definitions for media and web controls

--- BleKeyboard.cpp
+++ BleKeyboard.cpp
@@ -82,15 +82,15 @@ static const uint8_t _hidReportDescriptor[] = {
   USAGE(1),           0xE2,          //   USAGE (Mute)                ; bit 4: 16
   USAGE(1),           0xE9,          //   USAGE (Volume Increment)    ; bit 5: 32
   USAGE(1),           0xEA,          //   USAGE (Volume Decrement)    ; bit 6: 64
-  USAGE(2),           0x23, 0x02,    //   Usage (WWW Home)            ; bit 7: 128
-  USAGE(2),           0x94, 0x01,    //   Usage (My Computer) ; bit 0: 1
-  USAGE(2),           0x92, 0x01,    //   Usage (Calculator)  ; bit 1: 2
-  USAGE(2),           0x2A, 0x02,    //   Usage (WWW fav)     ; bit 2: 4
-  USAGE(2),           0x21, 0x02,    //   Usage (WWW search)  ; bit 3: 8
-  USAGE(2),           0x26, 0x02,    //   Usage (WWW stop)    ; bit 4: 16
-  USAGE(2),           0x24, 0x02,    //   Usage (WWW back)    ; bit 5: 32
-  USAGE(2),           0x83, 0x01,    //   Usage (Media sel)   ; bit 6: 64
-  USAGE(2),           0x8A, 0x01,    //   Usage (Mail)        ; bit 7: 128
+  USAGE(2), 0x42, 0x00, // Usage (Stand By / Sleep)      ; bit 7: 128
+  USAGE(2), 0x83, 0x01, // Usage (System Sleep)          ; bit 0: 1
+  USAGE(2), 0x81, 0x01, // Usage (System Power Down)     ; bit 1: 2
+  USAGE(2), 0x94, 0x01, // Usage (My Computer)           ; bit 2: 4
+  USAGE(2), 0x92, 0x01, // Usage (Calculator)            ; bit 3: 8
+  USAGE(2), 0x2A, 0x02, // Usage (WWW Bookmarks)         ; bit 4: 16
+  USAGE(2), 0x21, 0x02, // Usage (WWW Search)            ; bit 5: 32
+  USAGE(2), 0x26, 0x02, // Usage (WWW Stop)              ; bit 6: 64
+  USAGE(2), 0x24, 0x02, // Usage (WWW Back)              ; bit 7: 128
   HIDINPUT(1),        0x02,          //   INPUT (Data,Var,Abs,No Wrap,Linear,Preferred State,No Null Position)
   END_COLLECTION(0)                  // END_COLLECTION
 };
