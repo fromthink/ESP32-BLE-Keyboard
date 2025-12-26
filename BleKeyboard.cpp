05e7b9e2dcfeeb15f70af74bca85698d8748348f
fromthink <8228841@qq.com>
2025-12-26 15:09:55 +0800

    Update BleKeyboard.cpp
    test

--- BleKeyboard.cpp
+++ BleKeyboard.cpp
@@ -59,10 +59,10 @@ static const uint8_t _hidReportDescriptor[] = {
   REPORT_COUNT(1),    0x06,          //   REPORT_COUNT (6) ; 6 bytes (Keys)
   REPORT_SIZE(1),     0x08,          //   REPORT_SIZE(8)
   LOGICAL_MINIMUM(1), 0x00,          //   LOGICAL_MINIMUM(0)
-  LOGICAL_MAXIMUM(1), 0x65,          //   LOGICAL_MAXIMUM(0x65) ; 101 keys
+  LOGICAL_MAXIMUM(1), 0xE7,          //   LOGICAL_MAXIMUM(0xE7) ; 101 keys
   USAGE_PAGE(1),      0x07,          //   USAGE_PAGE (Kbrd/Keypad)
   USAGE_MINIMUM(1),   0x00,          //   USAGE_MINIMUM (0)
-  USAGE_MAXIMUM(1),   0x65,          //   USAGE_MAXIMUM (0x65)
+  USAGE_MAXIMUM(1),   0xE7,          //   USAGE_MAXIMUM (0xE7)
   HIDINPUT(1),        0x00,          //   INPUT (Data,Array,Abs,No Wrap,Linear,Preferred State,No Null Position)
   END_COLLECTION(0),                 // END_COLLECTION
   // ------------------------------------------------- Media Keys
@@ -77,20 +77,20 @@ static const uint8_t _hidReportDescriptor[] = {
   REPORT_COUNT(1),    0x10,          //   REPORT_COUNT (16)
   USAGE(1),           0xB5,          //   USAGE (Scan Next Track)     ; bit 0: 1
   USAGE(1),           0xB6,          //   USAGE (Scan Previous Track) ; bit 1: 2
-  USAGE(1),           0x30,          //   USAGE (Power)                ; bit 2: 4
-  USAGE(1),           0x32,          //   USAGE (Sleep)          ; bit 3: 8
+  USAGE(1),           0xB7,          //   USAGE (Stop)                ; bit 2: 4
+  USAGE(1),           0xCD,          //   USAGE (Play/Pause)          ; bit 3: 8
   USAGE(1),           0xE2,          //   USAGE (Mute)                ; bit 4: 16
   USAGE(1),           0xE9,          //   USAGE (Volume Increment)    ; bit 5: 32
   USAGE(1),           0xEA,          //   USAGE (Volume Decrement)    ; bit 6: 64
-  USAGE(2), 0x42, 0x00, // Usage (Stand By / Sleep)      ; bit 7: 128
-  USAGE(1),           0x30,  // Usage (System Sleep)          ; bit 0: 1
-  USAGE(1),           0x32,  // Usage (System Power Down)     ; bit 1: 2
-  USAGE(2), 0x94, 0x01, // Usage (My Computer)           ; bit 2: 4
-  USAGE(2), 0x92, 0x01, // Usage (Calculator)            ; bit 3: 8
-  USAGE(2), 0x2A, 0x02, // Usage (WWW Bookmarks)         ; bit 4: 16
-  USAGE(2), 0x21, 0x02, // Usage (WWW Search)            ; bit 5: 32
-  USAGE(2), 0x26, 0x02, // Usage (WWW Stop)              ; bit 6: 64
-  USAGE(2), 0x24, 0x02, // Usage (WWW Back)              ; bit 7: 128
+  USAGE(2),           0x23, 0x02,    //   Usage (WWW Home)            ; bit 7: 128
+  USAGE(2),           0x94, 0x01,    //   Usage (My Computer) ; bit 0: 1
+  USAGE(2),           0x92, 0x01,    //   Usage (Calculator)  ; bit 1: 2
+  USAGE(2),           0x2A, 0x02,    //   Usage (WWW fav)     ; bit 2: 4
+  USAGE(2),           0x21, 0x02,    //   Usage (WWW search)  ; bit 3: 8
+  USAGE(2),           0x26, 0x02,    //   Usage (WWW stop)    ; bit 4: 16
+  USAGE(2),           0x24, 0x02,    //   Usage (WWW back)    ; bit 5: 32
+  USAGE(2),           0x83, 0x01,    //   Usage (Media sel)   ; bit 6: 64
+  USAGE(2),           0x8A, 0x01,    //   Usage (Mail)        ; bit 7: 128
   HIDINPUT(1),        0x02,          //   INPUT (Data,Var,Abs,No Wrap,Linear,Preferred State,No Null Position)
   END_COLLECTION(0)                  // END_COLLECTION
 };
